d7458fdf5a3b69bd780b8bf0c83c92b08b3355ba
saman333 <samanraut.sr@gmail.com>
2023-07-17 13:33:47 +0545

    feat: use break statement in c#

--- Csharp_practice/Program.cs
+++ Csharp_practice/Program.cs
@@ -106,15 +106,7 @@
                 }
                 Console.Write(cars[i] + " ");
             }
-            Console.WriteLine("\n\nUsing continue statement");
-            for (int i = 0; i < cars.Length; i++)
-            {
-                if (i == 2)
-                {
-                    continue;
-                }
-                Console.Write(cars[i] + " ");
-            }
+            
         }
     }
 }
