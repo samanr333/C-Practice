acbc602bc61f0219f8882f9bc32d2a3dc9f7e94a
saman333 <samanraut.sr@gmail.com>
2023-07-17 16:42:36 +0545

    feat: use list of objects in c#

--- Csharp_practice/ArrayandList.cs
+++ Csharp_practice/ArrayandList.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +8,17 @@ namespace Csharp_practice
 {
     internal class ArrayandList
     {
+        //Defining list of object
+        public class Info
+        {
+            public string name { get; set; }
+            public int age { get; set; }
+        }
         static void Main(string[] args)
         {
             List<string> bikes = new List<string> { "Ducati", "Yamaha", "MV Agusta", "Kawasaki" };
             List<string> cars = new List<string> { "Lamborghini", "Mercedes", "Rolls Royace", "Bugatti" };
-            List<int> nums = new List<int> { 2, 6, 14, 24, 17, 333, 18, 45, 15 };
+            List<int> nums = new List<int> { 2, 6, 14, 24, 17, 18, 45, 15 };
             foreach(string bike in bikes)
             {
                 Console.Write(bike + ", ");
@@ -44,9 +50,20 @@ namespace Csharp_practice
                 Console.Write(car + ", ");
             }
             //Finding elements in the list
-            List<int> number = new List<int> nums.Find(n => n > 40);
+            int number = nums.Find(n => n > 20);
             Console.Write("\n\nElements in nums that are greater than 40: " + number);
+
+            //List of Objects
+            Console.WriteLine("\n\nList of objects:");
+            List<Info> players = new List<Info>();
+            players.Add(new Info { name = "AB De Villiers", age = 39 });
+            players.Add(new Info { name = "Virat Kohli", age = 34 });
+
+            foreach(Info player in players)
+            {
+                Console.WriteLine("Name: " + player.name + ", " + "Age: " + player.age);
+            }
+
         }
     }
 }
-*/
