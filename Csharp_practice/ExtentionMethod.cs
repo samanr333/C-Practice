fe064a530feae97201a7464e8cc804c9e6243de5
saman333 <samanraut.sr@gmail.com>
2023-07-19 11:00:03 +0545

    feat: use Extention methods to capitalize the string

--- Csharp_practice/ExtentionMethod.cs
+++ Csharp_practice/ExtentionMethod.cs
@@ -18,6 +18,15 @@ namespace Csharp_practice
             return number % 2 != 0;
         }
     }
+    public static class Capitalize
+    {
+        public static string StringCap(this string input)
+        {
+            if(string.IsNullOrEmpty(input)) 
+                return input;
+            return input.ToUpper();
+        }
+    }
     internal class ExtentionMethod
     {
         static void Main(string[] args)
@@ -27,9 +36,14 @@ namespace Csharp_practice
 
             Console.WriteLine($"{number1} is even: {number1.IsEven()}");
             Console.WriteLine($"{number1} is odd: {number1.IsOdd()}");
-
             Console.WriteLine($"{number2} is even: {number2.IsEven()}");
             Console.WriteLine($"{number2} is odd: {number2.IsOdd()}");
+
+            //String capitalizer
+            string name = "Zlatan";
+            string name1 = name.StringCap();
+            Console.WriteLine($"Before capitalizing: {name}");
+            Console.WriteLine($"After capitalizing: {name1}");
         }
     }    
 }
