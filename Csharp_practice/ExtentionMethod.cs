5c019d2d9e03f135f3e43825c818bb4e128fdadb
saman333 <samanraut.sr@gmail.com>
2023-07-19 10:44:51 +0545

    feat: use Extention methods in c#

---
+++ Csharp_practice/ExtentionMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_practice
+{
+    public static class Check
+    {
+        public static bool IsEven(this int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static bool IsOdd(this int number)
+        {
+            return number % 2 != 0;
+        }
+    }
+    internal class ExtentionMethod
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 41;
+            int number2 = 56;
+
+            Console.WriteLine($"{number1} is even: {number1.IsEven()}");
+            Console.WriteLine($"{number1} is odd: {number1.IsOdd()}");
+
+            Console.WriteLine($"{number2} is even: {number2.IsEven()}");
+            Console.WriteLine($"{number2} is odd: {number2.IsOdd()}");
+        }
+    }    
+}
