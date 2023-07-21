f0135032cc76cae345635ac74aacf39466dcc1c8
saman333 <samanraut.sr@gmail.com>
2023-07-21 14:25:58 +0545

    feat: use multiple delegate in c#

--- EventsandDelegates/Program.cs
+++ EventsandDelegates/Program.cs
@@ -1,6 +1,7 @@
-﻿/*namespace EventsandDelegates
+﻿namespace EventsandDelegates
 {
     public delegate void Calculation(double a, double b);
+    public delegate void Calculation1(double x);
     internal class Program
     {
         protected static void Add(double a, double b)
@@ -19,12 +20,22 @@
         {
             Console.WriteLine($"The quotient is: {a / b}");
         }
+        protected static void Square(double x)
+        {
+            double square = x * x;
+            Console.WriteLine("The square of {0} is {1}", x, square) ;
+        }
+        protected static void Cube(double x)
+        {
+            double cube = x * x * x;
+            Console.WriteLine("The cube of {0} is {1}", x, cube);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Enter two numbers");
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
-            Calculation calculation = new Calculation(Program.Add);
+            Calculation calculation = new Calculation(Add);
             calculation(a, b);
             calculation = Subtract;
             calculation(a, b);
@@ -32,6 +43,12 @@
             calculation(a, b);
             calculation = Division;
             calculation(a, b);
+            Console.WriteLine("Enter a number.");
+            double x = double.Parse(Console.ReadLine());
+            Calculation1 calculation1 = new Calculation1(Square);
+            calculation1(x);
+            calculation1 = Cube;
+            calculation1(x);
         }
     }
-}*/
+}
