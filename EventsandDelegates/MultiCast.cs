92c04f55a8842b47fea07b8d706a56cd193e1eef
saman333 <samanraut.sr@gmail.com>
2023-07-21 14:32:16 +0545

    feat: use multi cast delegate in c#

--- EventsandDelegates/MultiCast.cs
+++ EventsandDelegates/MultiCast.cs
@@ -1,8 +1,13 @@
-﻿/*namespace EventsandDelegates
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsandDelegates
 {
     public delegate void Calculation(double a, double b);
-    public delegate void Calculation1(double x);
-    internal class Program
+    internal class MultiCast
     {
         protected static void Add(double a, double b)
         {
@@ -23,12 +28,7 @@
         protected static void Square(double x)
         {
             double square = x * x;
-            Console.WriteLine("The square of {0} is {1}", x, square) ;
-        }
-        protected static void Cube(double x)
-        {
-            double cube = x * x * x;
-            Console.WriteLine("The cube of {0} is {1}", x, cube);
+            Console.WriteLine("The square of {0} is {1}", x, square);
         }
         static void Main(string[] args)
         {
@@ -36,19 +36,10 @@
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
             Calculation calculation = new Calculation(Add);
+            calculation += Subtract;
+            calculation += Multiplication;
+            calculation += Division;
             calculation(a, b);
-            calculation = Subtract;
-            calculation(a, b);
-            calculation = Multiplication;
-            calculation(a, b);
-            calculation = Division;
-            calculation(a, b);
-            Console.WriteLine("Enter a number.");
-            double x = double.Parse(Console.ReadLine());
-            Calculation1 calculation1 = new Calculation1(Square);
-            calculation1(x);
-            calculation1 = Cube;
-            calculation1(x);
         }
     }
-}*/
+}
